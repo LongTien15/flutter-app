3e272a4f5df2d49ef79b069bb8910052876f127b
LongTien15 <nguyentienlong.150800@gmail.com>
2022-07-08 10:49:44 +0700

    fix base code

--- lib/data/repository/api_repo.dart
+++ lib/data/repository/api_repo.dart
@@ -1,6 +1,11 @@
 import 'package:http/http.dart' as http;
 
-//get request
+/* 
+- get method request
+- params: 
+  + urlString:  api url dưới dạng string
+- return: raw data dưới dạng biến Response hoặc ném Exception nếu gặp lỗi   
+*/
 Future<http.Response> makeGetRequest(String urlString) async {
   try {
     final http.Response res = await http.get(Uri.parse(urlString));
@@ -10,9 +15,15 @@ Future<http.Response> makeGetRequest(String urlString) async {
   }
 }
 
-//post request
+/* 
+- post  method request
+- params: 
+  + urlString:  api url dưới dạng string
+  + bodyInJson: body json cần truyền dưới dạng string
+- return: raw data dưới dạng biến Response hoặc ném Exception nếu gặp lỗi   
+*/
 Future<http.Response> makePostRequest(
-    String urlString, Object bodyInJson) async {
+    String urlString, String bodyInJson) async {
   Map<String, String> headers = {'Content-type': "application/json"};
   try {
     final http.Response res = await http.post(Uri.parse(urlString),
@@ -23,9 +34,15 @@ Future<http.Response> makePostRequest(
   }
 }
 
-//put request
+/* 
+- put method request
+- params: 
+  + urlString:  api url dưới dạng string
+  + bodyInJson: body json cần truyền dưới dạng string
+- return: raw data dưới dạng biến Response hoặc ném Exception nếu gặp lỗi  
+*/
 Future<http.Response> makePutRequest(
-    String urlString, Object bodyInJson) async {
+    String urlString, String bodyInJson) async {
   Map<String, String> headers = {'Content-type': "application/json"};
   try {
     final http.Response res = await http.put(Uri.parse(urlString),
@@ -36,7 +53,13 @@ Future<http.Response> makePutRequest(
   }
 }
 
-//patch request
+/* 
+- patch method request  
+- params: 
+  - urlString:  api url dưới dạng string
+  - bodyInJson: body json cần truyền dưới dạng string
+- return: raw data dưới dạng biến Response hoặc ném Exception nếu gặp lỗi    
+*/
 Future<http.Response> makePatchRequest(
     String urlString, Object bodyInJson) async {
   Map<String, String> headers = {'Content-type': "application/json"};
@@ -49,7 +72,12 @@ Future<http.Response> makePatchRequest(
   }
 }
 
-//delete request
+/* 
+- delete method request  
+- params: 
+  - urlString:  api url dưới dạng string
+- return: raw data dưới dạng biến Response hoặc ném Exception nếu gặp lỗi    
+*/
 Future<http.Response> deleteRequest(String urlString) async {
   try {
     final http.Response res = await http.delete(Uri.parse(urlString));
